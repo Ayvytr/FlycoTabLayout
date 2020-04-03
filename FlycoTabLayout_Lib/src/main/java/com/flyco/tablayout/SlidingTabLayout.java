b4f41c5d8d65e8e0fb3f93fb92f019e85b494ccf
Ayvytr <ayvytr@outlook.com>
2020-04-04 07:06:16 +0800

    修复tl_textBold="SELECT"时字体加粗问题

--- FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/SlidingTabLayout.java
+++ FlycoTabLayout_Lib/src/main/java/com/flyco/tablayout/SlidingTabLayout.java
@@ -318,6 +318,8 @@ public class SlidingTabLayout extends HorizontalScrollView implements ViewPager.
                     tv_tab_title.getPaint().setFakeBoldText(true);
                 } else if (mTextBold == TEXT_BOLD_NONE) {
                     tv_tab_title.getPaint().setFakeBoldText(false);
+                } else if (mTextBold == TEXT_BOLD_WHEN_SELECT) {
+                    tv_tab_title.getPaint().setFakeBoldText(mCurrentTab == i);
                 }
             }
         }
